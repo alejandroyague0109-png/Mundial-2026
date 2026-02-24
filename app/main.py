7ae69f8cd2fd8f7987daef805cdd0bc1b8812a7c
Alejandro Yague <alejandroyague0109@gmail.com>
2026-02-24 15:44:25 -0300

    correcciones varias

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import RedirectResponse, FileResponse, , HTMLResponse
+from fastapi.responses import RedirectResponse, FileResponse, HTMLResponse
 from contextlib import asynccontextmanager
 from pathlib import Path
 from dotenv import load_dotenv
