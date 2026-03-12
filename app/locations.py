ec3afdc3d97c2b4b7628d635b05ef2d8e9f7d07b
Alejandro Yague <alejandroyague0109@gmail.com>
2026-03-12 15:19:44 -0300

    Expansión LATAM inclusión de todos los hispanohablantes

--- app/locations.py
+++ app/locations.py
@@ -167,10 +167,135 @@ URUGUAY = {
 }
 
 CHILE = {
-    "Región Metropolitana": ["Santiago Centro", "Providencia", "Las Condes", "Ñuñoa", "Maipú", "La Florida", "Vitacura", "Puente Alto", "La Reina", "Macul", "Peñalolén", "San Miguel", "Estación Central", "Quilicura"],
-    "Valparaíso": ["Valparaíso", "Viña del Mar", "Concón", "Quilpué", "Villa Alemana", "San Antonio", "Los Andes"],
-    "Biobío": ["Valparaíso", "Concepción", "Talcahuano", "San Pedro de la Paz", "Chiguayante", "Los Ángeles"],
-    "Otras Regiones": ["Arica y Parinacota", "Tarapacá", "Antofagasta", "Atacama", "Coquimbo", "O'Higgins", "Maule", "Ñuble", "Araucanía", "La Araucanía", "Los Ríos", "Los Lagos", "Aysén", "Magallanes"]
+    "Región Metropolitana": ["Santiago Centro", "Providencia", "Las Condes", "Ñuñoa", "Maipú", "La Florida", "Vitacura", "Puente Alto", "La Reina", "Macul", "Peñalolén", "San Miguel", "Estación Central", "Quilicura", "San Bernardo", "Pudahuel"],
+    "Valparaíso": ["Valparaíso", "Viña del Mar", "Concón", "Quilpué", "Villa Alemana", "San Antonio", "Los Andes", "Quillota", "Interior"],
+    "Biobío": ["Concepción", "Talcahuano", "San Pedro de la Paz", "Chiguayante", "Los Ángeles", "Coronel", "Hualpén", "Interior"],
+    "Antofagasta": ["Antofagasta", "Calama", "Tocopilla", "Interior"],
+    "Coquimbo": ["La Serena", "Coquimbo", "Ovalle", "Illapel", "Interior"],
+    "Maule": ["Talca", "Curicó", "Linares", "Constitución", "Interior"],
+    "Araucanía": ["Temuco", "Padre Las Casas", "Villarrica", "Angol", "Interior"],
+    "O'Higgins": ["Rancagua", "Machalí", "San Fernando", "Rengo", "Interior"],
+    "Los Lagos": ["Puerto Montt", "Osorno", "Castro", "Puerto Varas", "Interior"],
+    "Otras Regiones": ["Arica y Parinacota", "Tarapacá (Iquique)", "Atacama (Copiapó)", "Ñuble (Chillán)", "Los Ríos (Valdivia)", "Aysén", "Magallanes (Punta Arenas)"]
+}
+
+# app/locations.py
+# ... (tu código de ARGENTINA, URUGUAY y CHILE se mantiene igual arriba) ...
+
+# --- EXPANSIÓN HISPANOAMÉRICA ---
+
+MEXICO = {
+    "Ciudad de México (CDMX)": [
+        "Álvaro Obregón", "Azcapotzalco", "Benito Juárez", "Coyoacán", "Cuajimalpa de Morelos", 
+        "Cuauhtémoc", "Gustavo A. Madero", "Iztacalco", "Iztapalapa", "La Magdalena Contreras", 
+        "Miguel Hidalgo", "Milpa Alta", "Tláhuac", "Tlalpan", "Venustiano Carranza", "Xochimilco"
+    ],
+    "Estado de México": [
+        "Ecatepec de Morelos", "Nezahualcóyotl", "Toluca", "Naucalpan de Juárez", "Chimalhuacán", 
+        "Tlalnepantla de Baz", "Cuautitlán Izcalli", "Tecámac", "Ixtapaluca", "Atizapán de Zaragoza", 
+        "Tultitlán", "Valle de Chalco", "Metepec", "Huixquilucan", "Texcoco", "Interior"
+    ],
+    "Aguascalientes": ["Aguascalientes", "Jesús María", "Calvillo", "Interior"],
+    "Baja California": ["Tijuana", "Mexicali", "Ensenada", "Playas de Rosarito", "Tecate", "San Quintín"],
+    "Baja California Sur": ["La Paz", "Los Cabos (Cabo San Lucas/San José)", "Comondú", "Interior"],
+    "Campeche": ["Campeche", "Ciudad del Carmen", "Champotón", "Interior"],
+    "Chiapas": ["Tuxtla Gutiérrez", "Tapachula", "San Cristóbal de las Casas", "Comitán", "Interior"],
+    "Chihuahua": ["Ciudad Juárez", "Chihuahua", "Cuauhtémoc", "Delicias", "Hidalgo del Parral", "Interior"],
+    "Coahuila": ["Saltillo", "Torreón", "Monclova", "Piedras Negras", "Acuña", "Interior"],
+    "Colima": ["Manzanillo", "Colima", "Villa de Álvarez", "Tecomán", "Interior"],
+    "Durango": ["Durango", "Gómez Palacio", "Lerdo", "Interior"],
+    "Guanajuato": ["León", "Irapuato", "Celaya", "Salamanca", "Guanajuato", "San Miguel de Allende", "Silao", "Interior"],
+    "Guerrero": ["Acapulco", "Chilpancingo", "Iguala", "Zihuatanejo", "Taxco", "Interior"],
+    "Hidalgo": ["Pachuca", "Tulancingo", "Mineral de la Reforma", "Tizayuca", "Tula", "Interior"],
+    "Jalisco": ["Guadalajara", "Zapopan", "San Pedro Tlaquepaque", "Tlajomulco de Zúñiga", "Tonalá", "Puerto Vallarta", "El Salto", "Lagos de Moreno", "Interior"],
+    "Michoacán": ["Morelia", "Uruapan", "Zamora", "Lázaro Cárdenas", "Apatzingán", "Zitácuaro", "Interior"],
+    "Morelos": ["Cuernavaca", "Jiutepec", "Cuautla", "Temixco", "Yautepec", "Interior"],
+    "Nayarit": ["Tepic", "Bahía de Banderas", "Xalisco", "Compostela", "Interior"],
+    "Nuevo León": ["Monterrey", "Apodaca", "Guadalupe", "General Escobedo", "Juárez", "San Nicolás de los Garza", "Santa Catarina", "San Pedro Garza García", "García", "Interior"],
+    "Oaxaca": ["Oaxaca de Juárez", "San Juan Bautista Tuxtepec", "Salina Cruz", "Juchitán", "Huajuapan", "Interior"],
+    "Puebla": ["Puebla", "Tehuacán", "Cholula", "Atlixco", "San Martín Texmelucan", "Interior"],
+    "Querétaro": ["Querétaro", "San Juan del Río", "El Marqués", "Corregidora", "Interior"],
+    "Quintana Roo": ["Cancún (Benito Juárez)", "Playa del Carmen (Solidaridad)", "Chetumal (Othón P. Blanco)", "Cozumel", "Tulum", "Interior"],
+    "San Luis Potosí": ["San Luis Potosí", "Soledad de Graciano Sánchez", "Ciudad Valles", "Matehuala", "Interior"],
+    "Sinaloa": ["Culiacán", "Mazatlán", "Ahome (Los Mochis)", "Guasave", "Navolato", "Interior"],
+    "Sonora": ["Hermosillo", "Cajeme (Cd. Obregón)", "Nogales", "San Luis Río Colorado", "Navojoa", "Guaymas", "Interior"],
+    "Tabasco": ["Centro (Villahermosa)", "Cárdenas", "Comalcalco", "Macuspana", "Interior"],
+    "Tamaulipas": ["Reynosa", "Matamoros", "Nuevo Laredo", "Ciudad Madero", "Tampico", "Ciudad Victoria", "Interior"],
+    "Tlaxcala": ["Tlaxcala", "Apizaco", "Huamantla", "Chiautempan", "Interior"],
+    "Veracruz": ["Veracruz", "Xalapa", "Coatzacoalcos", "Córdoba", "Boca del Río", "Orizaba", "Poza Rica", "Minatitlán", "Interior"],
+    "Yucatán": ["Mérida", "Kanasín", "Valladolid", "Tizimín", "Progreso", "Interior"],
+    "Zacatecas": ["Zacatecas", "Guadalupe", "Fresnillo", "Jerez", "Interior"]
+}
+
+COLOMBIA = {
+    "Bogotá D.C.": ["Usaquén", "Chapinero", "Santa Fe", "San Cristóbal", "Usme", "Tunjuelito", "Bosa", "Kennedy", "Fontibón", "Engativá", "Suba", "Barrios Unidos", "Teusaquillo", "Los Mártires", "Antonio Nariño", "Puente Aranda"],
+    "Antioquia": ["Medellín", "Bello", "Itagüí", "Envigado", "Rionegro", "Sabaneta", "Apartadó", "Interior"],
+    "Valle del Cauca": ["Cali", "Palmira", "Buenaventura", "Tuluá", "Cartago", "Buga", "Jamundí", "Interior"],
+    "Atlántico": ["Barranquilla", "Soledad", "Malambo", "Sabanalarga", "Interior"],
+    "Bolívar": ["Cartagena", "Magangué", "Turbaco", "Interior"],
+    "Cundinamarca": ["Soacha", "Chía", "Zipaquirá", "Facatativá", "Fusagasugá", "Mosquera", "Interior"],
+    "Santander": ["Bucaramanga", "Floridablanca", "Girón", "Piedecuesta", "Barrancabermeja", "Interior"],
+    "Norte de Santander": ["Cúcuta", "Ocaña", "Villa del Rosario", "Los Patios", "Interior"],
+    "Magdalena": ["Santa Marta", "Ciénaga", "Zona Bananera", "Interior"],
+    "Tolima": ["Ibagué", "Espinal", "Melgar", "Interior"],
+    "Risaralda & Caldas & Quindío (Eje Cafetero)": ["Pereira", "Dosquebradas", "Manizales", "Armenia", "Interior"],
+    "Meta": ["Villavicencio", "Acacías", "Interior"],
+    "Huila": ["Neiva", "Pitalito", "Interior"],
+    "Nariño": ["Pasto", "Tumaco", "Ipiales", "Interior"],
+    "Otras Regiones": ["Boyacá", "Cauca", "Cesar", "Córdoba", "Sucre", "Chocó", "Casanare", "Resto del país"]
+}
+
+PERU = {
+    "Lima Metropolitana": ["Lima Centro", "Miraflores", "San Isidro", "Surco", "San Borja", "La Molina", "San Miguel", "Los Olivos", "San Martín de Porres", "San Juan de Lurigancho", "Comas", "Ate", "San Juan de Miraflores", "Villa El Salvador", "Chorrillos"],
+    "Callao": ["Callao", "Bellavista", "La Punta", "Carmen de la Legua", "Ventanilla"],
+    "La Libertad": ["Trujillo", "Víctor Larco Herrera", "Huanchaco", "Chepén", "Interior"],
+    "Arequipa": ["Arequipa", "Yanahuara", "Cayma", "Cerro Colorado", "Paucarpata", "Interior"],
+    "Piura": ["Piura", "Sullana", "Paita", "Talara", "Interior"],
+    "Lambayeque": ["Chiclayo", "José Leonardo Ortiz", "La Victoria", "Lambayeque", "Interior"],
+    "Cusco": ["Cusco", "San Sebastián", "Wanchaq", "Sicuani", "Interior"],
+    "Junín": ["Huancayo", "El Tambo", "Tarma", "Jauja", "Interior"],
+    "Áncash": ["Chimbote", "Nuevo Chimbote", "Huaraz", "Interior"],
+    "Ica": ["Ica", "Chincha Alta", "Pisco", "Interior"],
+    "Otras Regiones": ["Loreto (Iquitos)", "Cajamarca", "Puno", "Tacna", "San Martín", "Ucayali", "Huánuco", "Ayacucho", "Resto del país"]
+}
+
+VENEZUELA = {
+    "Gran Caracas": ["Libertador", "Chacao", "Sucre", "Baruta", "El Hatillo", "Los Teques"],
+    "Zulia": ["Maracaibo", "San Francisco", "Cabimas", "Ciudad Ojeda", "Interior"],
+    "Carabobo": ["Valencia", "San Diego", "Naguanagua", "Guacara", "Puerto Cabello"],
+    "Lara & Aragua": ["Maracay", "Barquisimeto", "Cabudare", "Turmero"],
+    "Otras Regiones": ["San Cristóbal", "Maturín", "Ciudad Guayana", "Barcelona", "Mérida", "Resto del país"]
+}
+
+ECUADOR = {
+    "Pichincha": ["Quito Norte", "Quito Sur", "Quito Centro", "Cumbayá", "Tumbaco", "Sangolquí", "Interior"],
+    "Guayas": ["Guayaquil Norte", "Guayaquil Sur", "Guayaquil Centro", "Samborondón", "Daule", "Durán", "Interior"],
+    "Azuay": ["Cuenca", "Gualaceo", "Interior"],
+    "Otras Regiones": ["Santo Domingo", "Machala", "Manta", "Portoviejo", "Ambato", "Loja", "Riobamba", "Resto del país"]
+}
+
+BOLIVIA = {
+    "La Paz": ["La Paz Centro", "Zona Sur", "Sopocachi", "Miraflores", "El Alto", "Interior"],
+    "Santa Cruz": ["Santa Cruz de la Sierra", "Montero", "Warnes", "Interior"],
+    "Cochabamba": ["Cochabamba", "Quillacollo", "Sacaba", "Interior"],
+    "Otras Regiones": ["Oruro", "Tarija", "Sucre", "Potosí", "Trinidad", "Cobija", "Resto del país"]
+}
+
+PARAGUAY = {
+    "Asunción": ["Centro", "Villa Morra", "Carmelitas", "Sajonia", "Barrio Obrero", "Otras zonas"],
+    "Central": ["San Lorenzo", "Luque", "Fernando de la Mora", "Lambaré", "Capiatá", "Mariano Roque Alonso", "Ñemby"],
+    "Alto Paraná": ["Ciudad del Este", "Hernandarias", "Presidente Franco", "Minga Guazú"],
+    "Otras Regiones": ["Encarnación", "Coronel Oviedo", "Pedro Juan Caballero", "Concepción", "Resto del país"]
+}
+
+CENTRAL_AMERICA_AND_CARIBBEAN = {
+    "Guatemala": ["Ciudad de Guatemala", "Mixco", "Villa Nueva", "Quetzaltenango", "Resto del país"],
+    "Costa Rica": ["San José", "Alajuela", "Cartago", "Heredia", "Resto del país"],
+    "Panamá": ["Ciudad de Panamá", "San Miguelito", "La Chorrera", "Arraiján", "David", "Resto del país"],
+    "El Salvador": ["San Salvador", "Santa Tecla", "Soyapango", "Santa Ana", "San Miguel", "Resto del país"],
+    "Honduras": ["Tegucigalpa", "San Pedro Sula", "Choloma", "La Ceiba", "Resto del país"],
+    "Nicaragua": ["Managua", "León", "Masaya", "Granada", "Resto del país"],
+    "Rep. Dominicana & Cuba": ["Santo Domingo", "Santiago", "La Habana", "Resto"]
 }
 
 # --- EL DICCIONARIO MAESTRO ---
@@ -178,13 +303,42 @@ CHILE = {
 LOCATIONS_BY_COUNTRY = {
     "AR": ARGENTINA,
     "UY": URUGUAY,
-    "CL": CHILE
+    "CL": CHILE,
+    "MX": MEXICO,
+    "CO": COLOMBIA,
+    "PE": PERU,
+    "VE": VENEZUELA,
+    "EC": ECUADOR,
+    "BO": BOLIVIA,
+    "PY": PARAGUAY,
+    "GT": CENTRAL_AMERICA_AND_CARIBBEAN, # Usamos el mismo para agrupar Centroamérica y el Caribe
+    "CR": CENTRAL_AMERICA_AND_CARIBBEAN,
+    "PA": CENTRAL_AMERICA_AND_CARIBBEAN,
+    "SV": CENTRAL_AMERICA_AND_CARIBBEAN,
+    "HN": CENTRAL_AMERICA_AND_CARIBBEAN,
+    "NI": CENTRAL_AMERICA_AND_CARIBBEAN,
+    "DO": CENTRAL_AMERICA_AND_CARIBBEAN,
+    "CU": CENTRAL_AMERICA_AND_CARIBBEAN
 }
 
 # --- LISTA PARA EL FRONTEND (Formulario de Registro) ---
-# Esto se enviará al select del HTML para que el usuario elija su país
 AVAILABLE_COUNTRIES = [
     {"code": "AR", "name": "Argentina", "flag": "🇦🇷", "prefix": "+54"},
+    {"code": "MX", "name": "México", "flag": "🇲🇽", "prefix": "+52"},
+    {"code": "CO", "name": "Colombia", "flag": "🇨🇴", "prefix": "+57"},
+    {"code": "PE", "name": "Perú", "flag": "🇵🇪", "prefix": "+51"},
+    {"code": "CL", "name": "Chile", "flag": "🇨🇱", "prefix": "+56"},
+    {"code": "VE", "name": "Venezuela", "flag": "🇻🇪", "prefix": "+58"},
+    {"code": "EC", "name": "Ecuador", "flag": "🇪🇨", "prefix": "+593"},
+    {"code": "BO", "name": "Bolivia", "flag": "🇧🇴", "prefix": "+591"},
+    {"code": "PY", "name": "Paraguay", "flag": "🇵🇾", "prefix": "+595"},
     {"code": "UY", "name": "Uruguay", "flag": "🇺🇾", "prefix": "+598"},
-    {"code": "CL", "name": "Chile", "flag": "🇨🇱", "prefix": "+56"}
+    {"code": "CR", "name": "Costa Rica", "flag": "🇨🇷", "prefix": "+506"},
+    {"code": "PA", "name": "Panamá", "flag": "🇵🇦", "prefix": "+507"},
+    {"code": "GT", "name": "Guatemala", "flag": "🇬🇹", "prefix": "+502"},
+    {"code": "SV", "name": "El Salvador", "flag": "🇸🇻", "prefix": "+503"},
+    {"code": "HN", "name": "Honduras", "flag": "🇭🇳", "prefix": "+504"},
+    {"code": "NI", "name": "Nicaragua", "flag": "🇳🇮", "prefix": "+505"},
+    {"code": "DO", "name": "Rep. Dominicana", "flag": "🇩🇴", "prefix": "+1"},
+    {"code": "CU", "name": "Cuba", "flag": "🇨🇺", "prefix": "+53"}
 ]
